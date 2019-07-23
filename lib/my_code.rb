cff6f5faa64f7103c8b099148ef6c2a76c115882
Emily G <aspentree79@gmail.com>
2019-07-23 20:47:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,19 +54,6 @@ def reduce_to_all_true(source_array)
 end
 
 
-# describe "reduce_to_all_true returns true when all values are truthy" do
-#   it "reduces correctly" do
-#     source_array = [1, 2, true, "razmatazz"]
-#     expect(reduce_to_all_true(source_array)).to be_truthy
-#   end
-# end
-#
-# describe "reduce_to_all_true returns false when any value is false" do
-#   it "reduces correctly" do
-#     source_array = [1, 2, true, "razmatazz", false]
-#     expect(reduce_to_all_true(source_array)).to be_falsy
-#   end
-# end
 def reduce_to_all_true(source_array)
 if source_array.any?(false)
     return false
